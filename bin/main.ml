c46cd8fa769956b8573785c20c4e256967861545
Kristoffer August Kortbaek <kristoffer@kortbaek.dk>
2024-06-18 11:58:19 +0200

    mostly report

--- bin/main.ml
+++ bin/main.ml
@@ -1,18 +1,17 @@
-[%%ml let mul_ml n m =
-        if [%lift 1 n] < [%lift 1 1]
+[%%ml let mul_ml_dynamic_if n m =
+        if [%lift 2 m] < [%lift 2 1]
         then [%lift 2 0]
         else
           [%add 2
-            [%lift 2 m]
-            [%app 2
-                (mul_ml
-                   [%sub 1 [%lift 1 n] [%lift 1 1]]
-                   [%lift 2 m])]]
+              [%lift 2 n]
+              [%app 2
+                  (mul_ml_dynamic_if                     
+                     [%lift 1 n]
+                     [%sub 2 [%lift 2 m] [%lift 2 1]])]]
 ]
-let mul_ml' = [%run mul_ml 7]
-let res = [%run mul_ml' 6]
+
+let res = [%run mul_ml_dynamic_if 7]
+let __res = [%run res 7]
+
 let () =
-  (* print_int @@ elm [0, 40, 41, 42, 43]; *)
-  ()
-
-
+  Printf.printf "Res. %d\n" (res 7);
