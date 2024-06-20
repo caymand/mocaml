978e10fc6c0e5a259aee090579f105e1bc769a90
Kristoffer August Kortbaek <kristoffer@kortbaek.dk>
2024-06-20 16:22:42 +0200

    almost done

--- bin/main.ml
+++ bin/main.ml
@@ -1,17 +1,50 @@
-[%%ml let mul_ml_dynamic_if n m =
-        if [%lift 2 m] < [%lift 2 1]
-        then [%lift 2 0]
-        else
-          [%add 2
-              [%lift 2 n]
-              [%app 2
-                  (mul_ml_dynamic_if                     
-                     [%lift 1 n]
-                     [%sub 2 [%lift 2 m] [%lift 2 1]])]]
+(* [%%ml let sum_scale s b a = *)
+(*         if [%lift 3 a] < [%lift 1 2 b] *)
+(*         then [%add 3 *)
+(*             [%mul 3 [%lift 2 1 s] [%lift 3 a]] *)
+(*             [%app 3 (sum_scale *)
+(*                        [%lift 1 s] *)
+(*                        [%lift 2 b] *)
+(*                        [%add 3 [%lift 3 1] [%lift 3 a]])] *)
+(*         ] *)
+(*         else [%lift 3 0] *)
+(* ] *)
+
+(* This is broken because the thing that gets "smaller" in the recursive call,
+   (argument a) doe not have the largest binding time. *)
+[%%ml let sum_scale_broken s a b =
+        if [%lift 1 2 a] < [%lift 3 b]
+        then [%add 3
+            [%lift 1 2 [%mul 2 [%lift 1 1 s] [%lift 2 a]]]
+            [%app 3 (sum_scale_broken
+                       [%lift 1 s]
+                       [%add 2 [%lift 2 1] [%lift 2 a]]
+                       [%lift 3 b])]
+        ]
+        else [%lift 3 0]
 ]
 
-let res = [%run mul_ml_dynamic_if 7]
-let __res = [%run res 7]
+[%%ml let sum_scale s a b =
+        if [%lift 3 b] < [%lift 1 2 a]
+        then [%lift 3 0]
+        else [%add 3
+            [%mul 3 [%lift 2 1 s] [%lift 3 b]]
+            [%app 3 (sum_scale
+                       [%lift 1 s]
+                       [%lift 2 a]
+                       [%sub 3 [%lift 3 1] [%lift 3 b]])]
+        ]        
+]
+
+let res = [%run sum_scale_broken 0]
+let res_broken = [%run res 0]
+
+let res1 = [%run sum_scale 2]
+let sum_to = [%run res1 0]
+let res3 = [%run sum_to 3]
 
 let () =
-  Printf.printf "Res. %d\n" (res 7);
+  let a = res1 0 3 in
+  let b = sum_to 3 in
+  Printf.printf "%d %d %d\n" a b res3;
+  ()
